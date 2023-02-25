9992455c39247c852ffcfb813d97ff62de210471
vigorjs <111201912289@mhs.dinus.ac.id>
2023-02-25 17:55:34 +0700

    first commit Javascript

---
+++ operator/ternary.js
@@ -0,0 +1,3 @@
+var pertanyaan = (50 - 60) == -10 ? "Kamu Benar" : "Kamu Salah"
+
+console.log(pertanyaan)
---
+++ variabel-tipedata/app.js
@@ -0,0 +1,24 @@
+// untuk membuat variabel ada 3 cara : var, let, const
+var nama = "Virgo Fajar Pamungkas"
+var usia = 21
+
+const angka1 = 20
+let angka2 = 20
+hasil = (angka1 + angka2)
+
+// document.write("Halo My Name is " + nama + "<br>")
+// document.write("Penjumlahan antara 20 + 20 = " + hasil)
+
+// for(let i = 1; i<6; i++){
+//     console.log(i)
+// }
+// console.log("ini "+ i)
+
+// Ketetapan Data
+const fav_food = "Bakso"
+fav_food = "Berubah"
+console.log(fav_food)
+
+// 1. Let dan Const menganut sistem block scope
+// 2. Var menganut sistem functional scope
+// 3. Data let dan var itu bisa diubah, sedangkan cons tidak bisa
